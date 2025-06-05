a62441546c0bc176565ea98fcdbadd57dd2f594a
Ricardo Filgueiras <107813151+Ricardo-Filgueiras@users.noreply.github.com>
2025-06-05 17:57:17 -0300

    Add data analysis features

--- App.py
+++ App.py
@@ -1,8 +1,12 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from ferramenta import criar_ferramentas
+from langchain.prompts import PromptTemplate 
+from langchain.agents import create_react_agent, AgentExecutor
+
 import streamlit as st
 import pandas as pd
 import os
-from ferramenta import *
+
 
 
 st.set_page_config(page_title="Assistente de análise de dados com IA", layout="centered")
@@ -27,7 +31,7 @@ st.markdown("### 📁 Faça upload do seu arquivo CSV")
 arquivo_carregado = st.file_uploader("Selecione um arquivo CSV", type="csv", label_visibility="collapsed")
 
 if arquivo_carregado:
-    df = pd.read_csv(arquivo_carregado)
+    df = pd.read_csv(arquivo_carregado , sep=";", encoding="utf-8")
     st.success("Arquivo carregado com sucesso!")
     st.markdown("### Primeiras linhas do DataFrame")
     st.dataframe(df.head())
@@ -88,3 +92,61 @@ if arquivo_carregado:
                                 handle_parsing_errors=True)
 
 
+  # AÇÕES RÁPIDAS
+    st.markdown("---")
+    st.markdown("## ⚡ Ações rápidas")
+
+    # Relatório de informações gerais
+    if st.button("📄 Relatório de informações gerais", key="botao_relatorio_geral"):
+        with st.spinner("Gerando relatório 🦜"):
+            resposta = orquestrador.invoke({"input": "Quero um relatório com informações sobre os dados", "df_head": df_head })
+            st.session_state['relatorio_geral'] = resposta["output"]
+
+    # Exibe o relatório com botão de download
+    if 'relatorio_geral' in st.session_state:
+        with st.expander("Resultado: Relatório de informações gerais"):
+            st.markdown(st.session_state['relatorio_geral'])
+
+            st.download_button(
+                label="📥 Baixar relatório",
+                data=st.session_state['relatorio_geral'],
+                file_name="relatorio_informacoes_gerais.md",
+                mime="text/markdown"
+            )
+
+    # Relatório de estatísticas descritivas
+    if st.button("📄 Relatório de estatísticas descritivas", key="botao_relatorio_estatisticas"):
+        with st.spinner("Gerando relatório 🦜"):
+            resposta = orquestrador.invoke({"input": "Quero um relatório de estatísticas descritivas", "df_head": df_head })
+            st.session_state['relatorio_estatisticas'] = resposta["output"]
+
+    # Exibe o relatório salvo com opção de download
+    if 'relatorio_estatisticas' in st.session_state:
+        with st.expander("Resultado: Relatório de estatísticas descritivas"):
+            st.markdown(st.session_state['relatorio_estatisticas'])
+
+            st.download_button(
+                label="📥 Baixar relatório",
+                data=st.session_state['relatorio_estatisticas'],
+                file_name="relatorio_estatisticas_descritivas.md",
+                mime="text/markdown"  
+            )
+   
+   # PERGUNTA SOBRE OS DADOS
+    st.markdown("---")
+    st.markdown("## 🔎 Perguntas sobre os dados")
+    pergunta_sobre_dados = st.text_input("Faça uma pergunta sobre os dados (ex: 'Qual é a média do tempo de entrega?')")
+    if st.button("Responder pergunta", key="responder_pergunta_dados"):
+        with st.spinner("Analisando os dados 🦜"):
+            resposta = orquestrador.invoke({"input": pergunta_sobre_dados , "df_head": df_head})
+            st.markdown((resposta["output"]))
+
+
+    # GERAÇÃO DE GRÁFICOS
+    st.markdown("---")
+    st.markdown("## 📊 Criar gráfico com base em uma pergunta")
+
+    pergunta_grafico = st.text_input("Digite o que deseja visualizar (ex: 'Crie um gráfico da média de tempo de entrega por clima.')")
+    if st.button("Gerar gráfico", key="gerar_grafico"):
+        with st.spinner("Gerando o gráfico 🦜"):
+            orquestrador.invoke({"input": pergunta_grafico , "df_head": df_head})
